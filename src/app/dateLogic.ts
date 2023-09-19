ad4ec78ae90c5e5877210855e4545bf398f84823
Lukas Polak <admin@lukaspolak.cz>
2023-09-19 17:16:45 +0200

    Frontend

---
+++ src/app/dateLogic.ts
@@ -0,0 +1,22 @@
+export const getDayBefore = (numberDate: number) => {
+    const date = new Date(numberDate);
+    date.setDate(date.getDate() - 1);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+};
+
+export const getTodayDate = () => {
+    const today = new Date();
+    // If it's before 6am, it's still yesterday
+    const hours = today.getHours();
+    if (hours < 6) {
+        return getDayBefore(today.getMilliseconds());
+    }
+
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    return `${month}/${day}/${year}`;
+};
--- src/pages/createMoodReport.tsx
+++ src/pages/createMoodReport.tsx
@@ -2,12 +2,19 @@
 
 import '../app/globals.css';
 import { checkSession } from "@/app/userLogic";
+import Image from 'next/image';
 import { useMemo, useState } from "react";
+import { getDayBefore, getTodayDate } from '@/app/dateLogic';
 
 export default function CreateMoodReport() {
 
     const [username, setUsername] = useState('');
     const [isInvalid, setIsInvalid] = useState(false);
+
+    const [mood, setMood] = useState(0);
+    const [date, setDate] = useState(getTodayDate());
+    const [comment, setComment] = useState('');
+    const [anythingNew, setAnythingNew] = useState('');
 
     useMemo(async () => {
         const user = await checkSession();
@@ -27,30 +34,56 @@ export default function CreateMoodReport() {
         </main>
     );
 
+    const handleMoodClick = (mood: number) => {
+        setMood(mood);
+    };
+
+    const imageNames = [1, 2, 3, 4, 5];
+
+    const handleChangeDate = () => {
+        // convert date from string to Date object
+        const dateObject = Date.parse(date);
+        setDate(getDayBefore(dateObject));
+    };
+
+    const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setComment(e.target.value);
+    };
+
+    const handleAnythingNewChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setAnythingNew(e.target.value);
+    };
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-12">
             <div className="justify-center text-center">
                 <h2 className="text-2xl font-bold mb-2">How are you feeling today, {username}?</h2>
                 <form>
+                    <h3 className="text-xl font-bold">Date</h3>
+                    Today, it is {date} (or too early the day after).<br />
+                    <span onClick={() => { handleChangeDate(); }} className="underline">Change to the day before</span><br />
+                    <span onClick={() => { setDate(getTodayDate()); }} className="underline">Change to today</span>
                     <h3 className="text-xl font-bold">Mood</h3>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
-                        type="button"
-                    >
-                        1
-                    </button>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
-                        type="button"
-                    >
-                        2
-                    </button>
-
+                    <div className="flex flex-row justify-center gap-3">
+                        {imageNames.map((imageName) => (
+                            <Image src={`/${imageName}.svg`} width={64} height={64} alt={imageName.toString()} key={imageName} onClick={(e) => handleMoodClick(imageName)} className={(mood === imageName) ? "border-double border-4 border-sky-500" : "none"} />
+                        ))}
+                    </div>
+                    <h3 className="text-xl font-bold">Comment</h3>
+                    <textarea className="border-2 border-gray-300 rounded-md p-2 w-full h-60" id="comment" onChange={(e) => handleCommentChange(e)} />
+                    <h3 className="text-xl font-bold">Did you learn anything new today?</h3>
+                    <textarea className="border-2 border-gray-300 rounded-md p-2 w-full h-36" id="anythingNew" onChange={(e) => handleAnythingNewChange(e)} />
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
                         type="button"
                     >
                         Submit
+                    </button>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4 ml-4"
+                        type="button"
+                    >
+                        List my reports
                     </button>
                 </form>
             </div>
