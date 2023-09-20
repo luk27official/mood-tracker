1e74dd8e0278aabf320502e13a404d2cb1beb067
Lukas Polak <admin@lukaspolak.cz>
2023-09-20 11:41:53 +0200

    Added a graph for the mood data

--- src/pages/listMoodReports.tsx
+++ src/pages/listMoodReports.tsx
@@ -5,12 +5,14 @@ import '../app/globals.css';
 import { checkSession, getAllReportsServer } from "@/app/userLogic";
 import { useMemo, useState } from "react";
 import Image from 'next/image';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 export default function MoodReportsList() {
 
     const [username, setUsername] = useState('');
     const [isInvalid, setIsInvalid] = useState(false);
     const [reports, setReports] = useState<Report[]>([]);
+    const [fetchFailed, setFetchFailed] = useState(false);
 
     useMemo(async () => {
         const user = await checkSession();
@@ -21,13 +23,25 @@ export default function MoodReportsList() {
         }
 
         const allReports = await getAllReportsServer().then((res) => {
+            res.forEach((element: Report) => {
+                element.date = element.date.substring(0, element.date.indexOf("T"));
+            });
             setReports(res);
+        }).catch((err) => {
+            console.log(err);
+            setFetchFailed(true);
         });
     }, []);
 
     if (isInvalid) return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <span className="text-xl font-bold">Wrong credentials.</span>
+        </main>
+    );
+
+    if (fetchFailed) return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <span className="text-xl font-bold">Something went wrong while fetching your reports. Try again.</span>
         </main>
     );
 
@@ -66,24 +80,41 @@ export default function MoodReportsList() {
             <span className='text-xl font-bold'>Hello, {username}!</span>
             <span>This is a list of your submitted reports. You can go back to the main page using <a href={"/"} className="text-red-500 underline cursor-pointer">this link</a>.</span>
             {reports.length !== 0 ?
-                <table className="">
-                    <tr>
-                        <th>Date</th>
-                        <th>Mood</th>
-                        <th>Comment</th>
-                        <th>Learned anything new?</th>
-                    </tr>
-                    {reports.map((report: Report) => {
-                        return (
-                            <tr key={report.date} className="text-center content-center">
-                                <td className="p-3">{report.date.substring(0, report.date.indexOf("T"))}</td>
-                                <td className="p-3"><Image src={`/${report.mood}.svg`} width={24} height={24} alt={report.mood.toString()} /></td>
-                                <td className="p-3">{report.comment}</td>
-                                <td className="p-3">{report.anythingNew}</td>
+                <div>
+                    <table className="">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Mood</th>
+                                <th>Comment</th>
+                                <th>Learned anything new?</th>
                             </tr>
-                        );
-                    })}
-                </table> : <span>No reports yet.</span>}
+                        </thead>
+                        <tbody>
+                            {reports.map((report: Report) => {
+                                return (
+                                    <tr key={report.date} className="text-center content-center">
+                                        <td className="p-3">{report.date}</td>
+                                        <td className="p-3"><Image src={`/${report.mood}.svg`} width={24} height={24} alt={report.mood.toString()} /></td>
+                                        <td className="p-3">{report.comment}</td>
+                                        <td className="p-3">{report.anythingNew}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    <ResponsiveContainer height={200}>
+                        <LineChart data={reports}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="date" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="mood" stroke="#8884d8" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+                : <span>No reports yet.</span>}
             <span>Also, you can download a list of your reports in JSON format by <span onClick={downloadReports} className="text-red-500 underline cursor-pointer">clicking here</span>.</span>
         </main>
     );
